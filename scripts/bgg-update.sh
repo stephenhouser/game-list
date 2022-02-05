754620c6b9ea76f3bf6ce1f9b92b0966cfe5ad41
Stephen Houser <stephenhouser@gmail.com>
2022-02-05 11:01:42 -0500

    add update script

---
+++ scripts/bgg-update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+BGG_USERNAME=0xACE
+
+curl -o dl-collection.xml https://boardgamegeek.com/xmlapi2/collection\?username\=${BGG_USERNAME}
+collection_lines=$(diff bgg-collection.xml dl-collection.xml | grep "^>" | wc -l)
+if [[ $collection_lines -gt 1 ]] ; then
+	mv dl-collection.xml bgg-collection.xml
+	git add bgg-collection.xml
+fi
+
+curl -o dl-plays.xml https://boardgamegeek.com/xmlapi2/plays\?username\=${BGG_USERNAME}
+play_lines=$(diff bgg-plays.xml dl-plays.xml | grep "^>" | wc -l)
+if [[ $play_lines -gt 1 ]] ; then
+	mv dl-plays.xml bgg-plays.xml
+	git add bgg-plays.xml
+fi
+
+staged=$(git diff --name-only --cached | wc -l)
+if [[ $staged -ge 1 ]] ; then
+	git commit -m"Update BGG game data."
+	git push origin master
+fi
