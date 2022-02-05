37cc57de3af6abf975c4fca9c773ba2089131175
Stephen Houser <stephenhouser@gmail.com>
2022-02-04 22:52:48 -0500

    merge non bgg games into listings

--- game-room.js
+++ game-room.js
@@ -2,8 +2,7 @@ const BGG_USER = '0xACE';
 
 function parsePlaysXML(xml) {
 	const plays = [];
-	const playsXML = (new window.DOMParser()).parseFromString(xml, "text/xml");
-	playsXML.querySelectorAll('play').forEach(function(play) {
+	xml.querySelectorAll('play').forEach(function(play) {
 		let players = [];
 		play.querySelectorAll('player').forEach(function(player) {
 			let name = player.getAttribute('name');
@@ -33,8 +32,7 @@ function parsePlaysXML(xml) {
 
 function parseGamesXML(xml) {
 	const games = [];
-	const gamesXML = (new window.DOMParser()).parseFromString(xml, "text/xml");
-	gamesXML.querySelectorAll('item').forEach(function(game) {
+	xml.querySelectorAll('item').forEach(function(game) {
 		let comments = '';
 		if (game.querySelector('comments')) {
 			comments = game.querySelector('comments').textContent;
@@ -61,22 +59,50 @@ function bgg_uri(game) {
 
 function bgg_image(game) {
 	const img_id = 'img_' + game.bgg_id;
-	$.ajax({
-		url: "https://boardgamegeek.com/xmlapi2/thing?id=" + game.bgg_id,
-	}).done(function(xml) {
-		const src = xml.querySelector('thumbnail').textContent;
-		$('.' + img_id).attr('src', src);
-	}).fail(function(err) {
-		console.log(err);
-	});
+	if (game.bgg_id > 0) {
+		$.ajax({
+			url: "https://boardgamegeek.com/xmlapi2/thing?id=" + game.bgg_id,
+		}).done(function(xml) {
+			const src = xml.querySelector('thumbnail').textContent;
+			$('.' + img_id).attr('src', src);
+		}).fail(function(err) {
+			console.log(err);
+		});
+	}
 
 	return '<img class="game-image ' + img_id + '" />';
 }
 
 function render_date(the_date) {
 	const d = new Date(the_date + "T00:00:00");
-	return the_date + ' '
+	return '<span style="white-space: nowrap;">' + the_date + '</span> '
 		+ d.toLocaleDateString('en-US', { weekday: 'long' });
+}
+
+function addLocalGames() {
+	debugger;
+	$.ajax({
+		url: 'games.xml',
+	}).done(function(xml) {
+		var game_list = $('#game-list').DataTable();
+		const games = parseGamesXML(xml);
+		game_list.rows.add(games).draw();
+	}).fail(function(err) {
+		console.log(err);
+	});
+}
+
+function addLocalPlays() {
+	debugger;
+	$.ajax({
+		url: 'plays.xml',
+	}).done(function(xml) {
+		var play_list = $('#play-list').DataTable();
+		const games = parsePlaysXML(xml);
+		play_list.rows.add(games).draw();
+	}).fail(function(err) {
+		console.log(err);
+	});
 }
 
 $(document).ready(function() {
@@ -85,7 +111,9 @@ $(document).ready(function() {
 		ajax: {
 			url: `https://boardgamegeek.com/xmlapi2/plays\?username\=${BGG_USER}`,
 			dataType: "text",
-			dataSrc: function(xml) {
+			dataSrc: function(response) {
+				addLocalPlays();
+				const xml = (new window.DOMParser()).parseFromString(response, "text/xml");
 				return parsePlaysXML(xml);
 			}
 		},
@@ -94,7 +122,7 @@ $(document).ready(function() {
 		columns: [
 			{ "data": "date" },
 			{ "data": null, "defaultContent": "", "orderable": false },
-			{ "data": "name" },
+			{ "data": "name", width: "25%"  },
 			{ "data": "players", "defaultContent": "" },
 			{ "data": "location", "defaultContent": "" },
 			{ "data": "comments", "defaultContent": "" }
@@ -130,7 +158,9 @@ $(document).ready(function() {
 		ajax: {
 			url: `https://boardgamegeek.com/xmlapi2/collection\?username\=${BGG_USER}`,
 			dataType: "text",
-			dataSrc: function(xml) {
+			dataSrc: function(response) {
+				addLocalGames();
+				const xml = (new window.DOMParser()).parseFromString(response, "text/xml");
 				return parseGamesXML(xml);
 			}
 		},
@@ -138,7 +168,7 @@ $(document).ready(function() {
 		order: [[1, "asc"]],
 		columns: [
 			{ "data": "image", "defaultContent": "", "orderable": false},
-			{ "data": "name" },
+			{ "data": "name", width: "25%" },
 			{ "data": "year", "defaultContent": "" },
 			{ "data": "plays", "defaultContent": "0" },
 			{ "data": "comments", "defaultContent": "" }
