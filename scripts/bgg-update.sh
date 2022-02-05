707d50e61b8e1657956a5c9307dc4be60f898682
Stephen Houser <stephenhouser@gmail.com>
2022-02-05 11:03:21 -0500

    script update

--- scripts/bgg-update.sh
+++ scripts/bgg-update.sh
@@ -6,6 +6,8 @@ collection_lines=$(diff bgg-collection.xml dl-collection.xml | grep "^>" | wc -l
 if [[ $collection_lines -gt 1 ]] ; then
 	mv dl-collection.xml bgg-collection.xml
 	git add bgg-collection.xml
+else
+	rm dl-collection.xml
 fi
 
 curl -o dl-plays.xml https://boardgamegeek.com/xmlapi2/plays\?username\=${BGG_USERNAME}
@@ -13,10 +15,12 @@ play_lines=$(diff bgg-plays.xml dl-plays.xml | grep "^>" | wc -l)
 if [[ $play_lines -gt 1 ]] ; then
 	mv dl-plays.xml bgg-plays.xml
 	git add bgg-plays.xml
+else
+	rm dl-plays.xml
 fi
 
 staged=$(git diff --name-only --cached | wc -l)
 if [[ $staged -ge 1 ]] ; then
-	git commit -m"Update BGG game data."
-	git push origin master
+	echo git commit -m"Update BGG game data."
+	echo git push origin master
 fi
